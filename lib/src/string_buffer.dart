7be017e3963cfdf62746347972c63bdf9793a9ea
ValentinVignal <valentin.vignal.dev@outlook.fr>
2021-07-23 23:16:42 +0800

    :art: Use spaces instead of tabs

--- lib/src/string_buffer.dart
+++ lib/src/string_buffer.dart
@@ -3,7 +3,7 @@ part of theme_color_palette;
 /// String buffer extension
 extension StringBufferExtension on StringBuffer {
   /// Indent from int
-  static String indent(int indent) => '\t' * indent;
+  static String indent(int indent) => '  ' * indent;
 
   /// Writes a line with a specific indentation
   void writeLine(int indentValue, String line) {
