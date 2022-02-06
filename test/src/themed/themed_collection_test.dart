51ec5b09b606afe1c13c05f68bf6ed57fa23b9bc
ValentinVignal <valentin.vignal.dev@outlook.fr>
2022-02-06 16:40:56 +0800

    :white_check_mark: Platform tests

---
+++ test/src/themed/themed_collection_test.dart
@@ -0,0 +1,46 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:theme_color_palette/src/theme_color_palette.dart';
+import 'package:theme_color_palette/src/utils/utils.dart';
+
+void main() {
+  tearDown(Themes.platforms.clear);
+  test(
+    'When the children does not have any platforms specified, it should take the same ones as the parents',
+    () {
+      Themes.platforms.addAll([
+        'platform0',
+        'platform1',
+        'platform2',
+      ]);
+      final parent = ThemedCollection.fromJson(
+        json: {
+          '.platforms': [
+            'platform0',
+            'platform1',
+          ], // The constructed collection does not include all the platforms.
+          'child': <String, dynamic>{}
+        },
+        context: BuildContext(
+          names: ['parentName'],
+          platforms: [
+            'platform0',
+            'platform1',
+            'platform2',
+          ], // The context given by the parent contains all the platforms.
+        ),
+      );
+      expect(
+        parent.context.platforms,
+        orderedEquals(['platform0', 'platform1']),
+        reason:
+            'The constructed collection should only contain the platforms that were specified',
+      );
+      expect(
+        parent.collections.first.context.platforms,
+        orderedEquals(['platform0', 'platform1']),
+        reason:
+            'The child collection should only contain the platforms that were specified in its parent',
+      );
+    },
+  );
+}
---
+++ test/src/themed/themed_json_to_dart_test.dart
@@ -0,0 +1,73 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:theme_color_palette/src/theme_color_palette.dart';
+import 'package:theme_color_palette/src/utils/utils.dart';
+
+class _TestThemedJsonToDart extends ThemedJsonToDart {
+  _TestThemedJsonToDart({
+    required Map<String, dynamic> json,
+    required BuildContext context,
+  }) : super(
+          json: json,
+          context: context,
+        );
+
+  @override
+  String get className => 'className';
+
+  @override
+  String toJsonString() => 'toJsonString';
+
+  @override
+  String fromJsonString({required String value, required String platform}) =>
+      'fromJsonString($value, $platform)';
+}
+
+void main() {
+  tearDown(() {
+    Themes.platforms.clear();
+    Themes.themes.clear();
+  });
+  test(
+    'It should only construct the object for the specified platforms',
+    () {
+      Themes.platforms.addAll([
+        'platform0',
+        'platform1',
+        'platform2',
+      ]);
+      final themedJsonToDart = _TestThemedJsonToDart(
+        json: {},
+        context: BuildContext(
+          names: ['parentNames'],
+          platforms: ['platform0', 'platform1'], // platform2 is not included.
+        ),
+      );
+
+      final dartDefine = themedJsonToDart.dartDefine(const DartDefineContext());
+
+      expect(
+        dartDefine,
+        contains('class className {'),
+        reason: 'It should create a generic class',
+      );
+
+      expect(
+        dartDefine,
+        contains('class className_platform0 implements className {'),
+        reason: 'It should create the platform0 class',
+      );
+
+      expect(
+        dartDefine,
+        contains('class className_platform1 implements className {'),
+        reason: 'It should create the platform1 class',
+      );
+
+      expect(
+        dartDefine,
+        isNot(contains('class className_platform2 implements className {')),
+        reason: 'It should not create the platform2 class',
+      );
+    },
+  );
+}
