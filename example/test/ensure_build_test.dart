7432bca274f93aeb5f18a5bb789dc7fc8b8d2e1e
ValentinVignal <valentin.vignal.dev@outlook.fr>
2025-08-05 23:26:48 +0800

    Unskip test

--- example/test/ensure_build_test.dart
+++ example/test/ensure_build_test.dart
@@ -2,8 +2,6 @@ import 'package:build_verify/build_verify.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test(
-      skip: true,
-      'ensure_build',
+  test('ensure_build',
       () async => expectBuildClean(packageRelativeDirectory: 'example'));
 }
